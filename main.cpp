eb7b3f9024cf7c2ed83b54d0487ce03203daad96
Yang Hu <yuexiachanming@gmail.com>
2020-12-04 17:36:09 -0600

    Add extractor impl, declar, demo main

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// main.cpp
+// This code reads a foreground.jpg and a background.jpg images from disk, manipulate
+// them using OpenCV, then writes the output to disk.
+// Author: Yang Hu
+
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include "extractor.h"
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    String folderpath = "archive/";
+    vector<String> filenames;
+    cv::glob(folderpath, filenames);
+    Extractor ext(true);
+    for (size_t i = 0; i < filenames.size(); i++)//filenames.size(); i++)
+    {
+        cout << filenames[i];
+        Mat img = imread(filenames[i]);
+        if (ext.extract(img)) {
+            imwrite("results/" + to_string(i) + "_rank.png", ext.rank);
+            imwrite("results/" + to_string(i) + "_suit.png", ext.suit);
+        }
+        else {
+            cout << "Error: cannot extract number and/or suit from image";
+            return 1;
+        }
+    }
+    return 0;
+}
